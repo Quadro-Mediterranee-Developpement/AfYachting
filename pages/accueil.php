50f1098a585aa8f7a4007d538835ccea9120584e
Seynokan <46873683+Seynokan@users.noreply.github.com>
2020-03-13 15:04:48 +0100

    Changement titre page accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -18,7 +18,7 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
         $i = end($next_buff);
     }
 
-    head($included, "Description de la page d'accueil adaptée au référencement", "Titre de la page d'accueil adaptée au référencement");
+    head($included, "Description de la page d'accueil adaptée au référencement", "AfYachting Locations et vente de bateaux à St Tropez");
     ?>
 
     <body>
