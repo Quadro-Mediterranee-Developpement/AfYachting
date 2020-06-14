d3ee2dbdbf23ec8e98b6f5be84c6ac981a07ae35
Hugo-creator-dev <hugo.musoles@free.fr>
2020-06-14 18:04:31 +0200

    ouf zaver oublier la bdd

--- afyachtingv2.sql
+++ afyachtingv2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3308
--- Généré le :  mer. 10 juin 2020 à 12:23
+-- Généré le :  Dim 14 juin 2020 à 15:53
 -- Version du serveur :  8.0.18
 -- Version de PHP :  7.3.12
 
@@ -45,8 +45,7 @@ CREATE TABLE IF NOT EXISTS `admin` (
 --
 
 INSERT INTO `admin` (`ID`, `Username`, `Password`, `Mail`, `Creation`, `Phone`, `valide`) VALUES
-(1, 'Admin@12', '1ee6426845a223d3622f39b86a7efadfd6e41dc5', 'Admin@12.com', '2020-06-02 22:00:00', '0652565620', '0'),
-(2, 'Admin', '1ee6426845a223d3622f39b86a7efadfd6e41dc5', 'Admin@12gg.Com', '2020-06-09 06:41:59', '0656822465', '??^n?U?');
+(1, 'Admin@12', '1ee6426845a223d3622f39b86a7efadfd6e41dc5', 'Admin@12.com', '2020-06-02 22:00:00', '0652565620', '0');
 
 -- --------------------------------------------------------
 
@@ -77,7 +76,7 @@ CREATE TABLE IF NOT EXISTS `bateau` (
 --
 
 INSERT INTO `bateau` (`ID`, `Description`, `Nom`, `Modele`, `Passagers`, `Moteur`, `Longueur`, `Equipement`, `Divers`, `Password`, `ID_Images`, `ID_Vente`, `ID_Location`) VALUES
-(1, 'gg', 'gg', 'gg', 4, 'gg', 4, 'gg', 'gg', 1, 1, 0, 0);
+(1, 'un grand bateau', 'LeGrandLarge', 'flotant', 5, 'turboBoost56', 65, 'coque;voile;frigo;', 'c\'est un bat', 1, 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -95,28 +94,14 @@ CREATE TABLE IF NOT EXISTS `client` (
   `Phone` varchar(25) DEFAULT NULL,
   `valide` text CHARACTER SET latin1 COLLATE latin1_swedish_ci,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `client`
 --
 
 INSERT INTO `client` (`ID`, `Username`, `Password`, `Mail`, `Creation`, `Phone`, `valide`) VALUES
-(1, 'hugohuu', NULL, 'hugo.hugoo@gmail.com', NULL, '', '0'),
-(2, 'hugoop', NULL, 'hugoop@gmail.com', NULL, '', '0'),
-(3, 'hugoope', NULL, 'hugoop@gmail.come', NULL, '', '0'),
-(4, 'hugoopee', NULL, 'hugoop@gmail.comee', NULL, '', '0'),
-(5, 'efrgthy', NULL, 'unemail@email.com', NULL, '', '0'),
-(6, 'hughh', NULL, 'hughh@hughh.com', NULL, '', '0'),
-(7, 'yy', NULL, 'yy.yy@gmail.com', '2020-06-05 12:07:52', '', '0'),
-(8, 'hugo', NULL, 'f.g@gmail.com', '2020-06-08 10:02:25', '', '0'),
-(9, 'H1@gmail.com', 'cca5153240d5f5d61ea46258bf47fec12b547e98', 'H1@gmail.com', '2020-06-08 10:03:54', '', '0'),
-(10, 'H1', 'cca5153240d5f5d61ea46258bf47fec12b547e98', 'H1@gmail.com', '2020-06-08 10:04:09', '', '0'),
-(11, 'hugo9', NULL, 'H1@gmail.co', '2020-06-08 10:05:09', '', '0'),
-(12, 'huho', NULL, 'huho@huho.fr', '2020-06-08 10:17:15', '', '0'),
-(13, 'hhhh', NULL, 'hugo.hugo@gmail.com', '2020-06-08 16:44:37', '', '0'),
-(14, 'New@12.00', NULL, 'New@12.com', '2020-06-08 16:52:42', '', '0'),
-(15, 'Pop20.salut', NULL, 'hugo.musoles@gmail.com', '2020-06-08 20:34:19', '', '0');
+(16, 'Client@12', 'a12e173daeb63ccd9d5fd657110d3238a8c77ac8', 'Client@12.com', '2020-06-14 14:00:10', '0606060606', '0');
 
 -- --------------------------------------------------------
 
@@ -132,15 +117,7 @@ CREATE TABLE IF NOT EXISTS `client_ponctuel` (
   `Creation` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `Phone` varchar(25) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `client_ponctuel`
---
-
-INSERT INTO `client_ponctuel` (`ID`, `Username`, `Mail`, `Creation`, `Phone`) VALUES
-(1, 'hugo', 'hugo.hugo@gmail.Com', NULL, NULL),
-(2, 'hugo', 'hugo@gmail.com', '2020-06-05 12:29:46', NULL);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -165,18 +142,14 @@ CREATE TABLE IF NOT EXISTS `evenement` (
   `ID_Bateau` int(11) NOT NULL,
   `ID_routageoptionevenement` int(11) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `evenement`
 --
 
 INSERT INTO `evenement` (`ID`, `Start`, `Stop`, `Total`, `State`, `Note`, `Start_Override`, `Stop_Override`, `ID_Admin`, `ID_Skipper`, `ID_Client`, `ID_Client_Ponctuel`, `ID_Bateau`, `ID_routageoptionevenement`) VALUES
-(11, '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL, 'zertyu', 'zertyukil', '2020-06-09 10:52:00', '2020-06-09 10:52:00', 0, 0, 0, 0, 0, 1),
-(12, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '555.000', 'klmù', 'hujikolpm', '2020-06-09 13:08:00', '2020-06-09 13:08:00', 1, 1, 1, 0, 0, 0),
-(13, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '5956.000', 'un salut', 'whechfergthyjuil', '2020-06-09 14:08:00', '2020-06-09 14:08:00', 1, 1, 8, 1, 0, 0),
-(14, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0.000', 'fffffff', '', '2020-06-09 20:52:00', '2020-06-18 20:52:00', 0, 0, 0, 0, 0, 0),
-(15, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '5555.000', 'teste3', 'oui non peut etre', '2020-06-11 12:42:00', '2020-06-11 12:42:00', 2, 1, 15, 1, 1, 0);
+(1, '0000-00-00 00:00:00', '0000-00-00 00:00:00', '559562.000', 'départ en bateau', 'rdv skipper 7h devant le port + embarcation 8h', '2020-06-23 06:00:00', '2020-06-23 12:00:00', 1, 2, 16, 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -198,8 +171,8 @@ CREATE TABLE IF NOT EXISTS `images` (
 --
 
 INSERT INTO `images` (`ID`, `Url`, `Alt_Description`, `ID_select`) VALUES
-(1, 'boat1.png', 'hh', 1),
-(2, 'boat2.png', 'oo', 1);
+(1, 'boat1.png', 'un beau bateau', 1),
+(2, 'boat2.png', 'un beau bateau', 1);
 
 -- --------------------------------------------------------
 
@@ -215,7 +188,14 @@ CREATE TABLE IF NOT EXISTS `location` (
   `BS` decimal(15,3) DEFAULT NULL,
   `Caution` decimal(15,3) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `location`
+--
+
+INSERT INTO `location` (`ID`, `HS`, `MS`, `BS`, `Caution`) VALUES
+(1, '120.000', '110.000', '100.000', '500.000');
 
 -- --------------------------------------------------------
 
@@ -237,8 +217,8 @@ CREATE TABLE IF NOT EXISTS `option_boat` (
 --
 
 INSERT INTO `option_boat` (`ID`, `Name`, `Description`, `Prix`) VALUES
-(1, 'yo', 'yooo', 5),
-(2, 'oui', 'non', 6);
+(1, 'volant', 'c\'est pratique pour conduire', 235),
+(2, 'ocean', 'c\'est en option', 12);
 
 -- --------------------------------------------------------
 
@@ -250,15 +230,14 @@ DROP TABLE IF EXISTS `routageimage`;
 CREATE TABLE IF NOT EXISTS `routageimage` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `routageimage`
 --
 
 INSERT INTO `routageimage` (`ID`) VALUES
-(1),
-(2);
+(1);
 
 -- --------------------------------------------------------
 
@@ -268,16 +247,19 @@ INSERT INTO `routageimage` (`ID`) VALUES
 
 DROP TABLE IF EXISTS `routageoption`;
 CREATE TABLE IF NOT EXISTS `routageoption` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ID_Bateau` int(11) NOT NULL,
-  `ID_Option` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `ID_Option` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `routageoption`
 --
 
-INSERT INTO `routageoption` (`ID_Bateau`, `ID_Option`) VALUES
-(1, 1);
+INSERT INTO `routageoption` (`ID`, `ID_Bateau`, `ID_Option`) VALUES
+(1, 1, 1),
+(2, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -287,16 +269,11 @@ INSERT INTO `routageoption` (`ID_Bateau`, `ID_Option`) VALUES
 
 DROP TABLE IF EXISTS `routageoptionevenement`;
 CREATE TABLE IF NOT EXISTS `routageoptionevenement` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
   `ID_Evenement` int(11) NOT NULL,
-  `ID_Option` int(11) NOT NULL
+  `ID_Option` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Déchargement des données de la table `routageoptionevenement`
---
-
-INSERT INTO `routageoptionevenement` (`ID_Evenement`, `ID_Option`) VALUES
-(1, 2);
 
 -- --------------------------------------------------------
 
@@ -314,14 +291,14 @@ CREATE TABLE IF NOT EXISTS `skipper` (
   `Phone` varchar(25) DEFAULT NULL,
   `valide` text,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `skipper`
 --
 
 INSERT INTO `skipper` (`ID`, `Username`, `Password`, `Mail`, `Creation`, `Phone`, `valide`) VALUES
-(1, 'Skipper@12.', '9b9ffaa57a745f661ed7e9ceace1a48ec8daf802', 'Skipper@12.com', '2020-06-09 06:43:23', '6666666666', '0');
+(2, 'Skipper@12', '693e0eb587013a66c8f315411737469229ad4111', 'Skipper@12.com', '2020-06-14 13:59:17', '0606060606', '0');
 
 -- --------------------------------------------------------
 
@@ -355,7 +332,14 @@ CREATE TABLE IF NOT EXISTS `vente` (
   `Largeur` float DEFAULT NULL,
   `Prix` decimal(15,3) DEFAULT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `vente`
+--
+
+INSERT INTO `vente` (`ID`, `Age`, `State`, `Largeur`, `Prix`) VALUES
+(1, 2012, 'tout neuf', 32, '55658.000');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
