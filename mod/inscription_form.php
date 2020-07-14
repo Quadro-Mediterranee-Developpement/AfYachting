3f6d19629a53b280bdfd5944b6ce9ee9881db61d
Seynokan <46873683+Seynokan@users.noreply.github.com>
2020-07-14 18:19:14 +0200

    Update inscription_form.php

--- mod/inscription_form.php
+++ mod/inscription_form.php
@@ -4,6 +4,7 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
     exit();
 } else {
     require_once 'utilityPhp/creationFormType.php';
+
     //TEXT
     // un peu partout
     function inscription_form($mode = false) {
@@ -17,15 +18,18 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
 
                 creationFormType::input_text("email", "inputEmail", "Adresse Email", "mail", "Adresse Email");
 
-                creationFormType::input_text("password", "inputPassword", "Mot de passe", "password", "Mot de passe <a class='small' href='https://www.economie.gouv.fr/particuliers/creer-mot-passe-securise'>(Doit respecter ce format)</a>");
+                creationFormType::input_text("password", "inputPassword", "Mot de passe", "password", "Mot de passe");
 
                 creationFormType::input_text("password", "inputConfirmPassword", "Confirmer mot de passe", "passwordVerif", "Confirmer mot de passe");
                 ?>
                 <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="inscript">Inscription</button>
                 <?php if (isset($_SESSION['erreur'])): ?>
                     <p id="errorform" class="form-control is-invalid"><?= $_SESSION['erreur']['desc']; ?></p>
-                <?php endif; ?>
+        <?php endif; ?>
                 <p id="errorform" class="form-control is-invalid" style="display: none"></p>
+                <div class="alert alert-primary" role="alert">
+                    Votre mot de passe doit contenir au moins 8 caractères dont 1 minuscule, 1 majuscule et 1 caractère spécial
+                </div>
                 <a class="d-block text-center mt-2 small" href="index?p=connexion">Connexion</a>
 
                 <script src="ajaxUse/XHR.js" type="text/javascript"></script>
